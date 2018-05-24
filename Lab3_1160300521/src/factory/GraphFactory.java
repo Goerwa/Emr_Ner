1a25153b2c99182fae06c6897d8d36c7da4e28c4
goerwa <wqsnzszw@163.com>
2018-05-24 22:23:54 +0800

    first

--- Lab3_1160300521/src/factory/GraphFactory.java
+++
@@ -1,9 +0,0 @@
-package factory;
-
-import edge.Edge;
-import graph.Graph;
-import vertex.Vertex;
-
-abstract class GraphFactory {
-	abstract public Graph<Vertex, Edge> createGraph(String filePath);
-}
