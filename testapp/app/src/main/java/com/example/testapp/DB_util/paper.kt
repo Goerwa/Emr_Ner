63424659975669ae9c226af8369b2eb220796cb5
goerwa <wqsnzszw@163.com>
2020-03-17 18:15:48 +0800

    first

--- testapp/app/src/main/java/com/example/testapp/DB_util/paper.kt
+++
@@ -1,8 +0,0 @@
-package com.example.testapp.DB_util
-
-class paper {
-    var paper_id:Int = 0
-    var paper_name = ""
-    var paper_q_id = ""
-    var paper_q_type = ""
-}
--- testapp/app/src/main/java/com/example/testapp/Data_type/Paper_test.kt
+++
@@ -1,8 +0,0 @@
-package com.example.testapp.Data_type
-
-class Paper_test {
-    var test_id = 0
-    var test_name = ""
-    var test_type = ""
-    var score = 0
-}
